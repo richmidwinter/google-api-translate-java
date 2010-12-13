dde6bcf8f00916c27bd02d4d965a147988f83338
rich <rich.midwinter@gmail.com>
2010-12-13 20:20:16 +0000

    Inserted LGPL licence text.

--- src/main/java/com/google/api/GoogleAPI.java
+++ src/main/java/com/google/api/GoogleAPI.java
@@ -2,21 +2,21 @@
  * GoogleAPI.java
  *
  * Copyright (C) 2009,  Richard Midwinter
- * 
+ *
  * This file is part of google-api-translate-java.
  *
- * google-api-translate-java is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
+ * google-api-translate-java is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
  * google-api-translate-java is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with google-api-translate-java.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with google-api-translate-java. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.google.api;
 
